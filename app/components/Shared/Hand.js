80d1f69bccf59909bdaac44e518d49f8e35f7386
danhollick <dan.hollick@gmail.com>
2017-06-04 18:02:52 +0200

    Added animation to Hand

--- app/components/Shared/Hand.js
+++ app/components/Shared/Hand.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import tachyons from 'styled-components-tachyons';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import 'variables.css';
+
+const rotate360 = keyframes`
+	0% {
+		transform: rotate(0deg);
+	}
+
+	60% {
+		transform: rotate(20deg);
+	}
+  100% {
+		transform: rotate(0deg);
+	}
+`;
 
 const Logo = styled.img`
   ${tachyons}
+  animation: ${rotate360} 0.8s ease-in-out infinite;
 `;
 
 export default Logo;
