be41edd911dfa2e9f6d64414bb059c4129bdbcd3
danhollick <dan.hollick@gmail.com>
2017-08-03 11:18:59 +0200

    Fixed some more SVG weirdness

--- app/components/Shared/RandomCircles.js
+++ app/components/Shared/RandomCircles.js
@@ -11,13 +11,14 @@ const CircleText = styled.text`
 
 const Circle = styled.svg`
   ${tachyons}
+   fill: var(--light-purple);
 `;
 
 export default class RandomCircles extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <Wrapper right_0 top_1_ns top_0 absolute db_ns dn >
-        <Circle height="400" width="400">
+        <Circle height="400" width="400" primary>
           <circle cx="200" cy="200" r="160" fill="#FFF7CE" opacity="0.7"/>
           <circle cx="140" cy="140" r="120" fill="#DCD6FF" opacity="0.7"/>
           <CircleText light_purple b x="10%" y="32%" text-anchor="middle"> Random shapes are totally</CircleText>
