973e718365895d437cef29bdf32a13179e57ffce
danhollick <dan.hollick@gmail.com>
2017-08-03 11:42:43 +0200

    Fixed the final bug. Will never need to work on this again.
    lol

--- app/containers/AboutPage/index.js
+++ app/containers/AboutPage/index.js
@@ -24,6 +24,11 @@ import Logo from 'components/Shared/Logo.js';
 import logo from '/Users/DanHollick/dev/alcohollick/app/assets/simplePortrait@2x.png';
 import SocialLinks from 'components/Shared/SocialLinks.js';
 
+const FlexCont = styled.div`
+  ${tachyons}
+  display : inline-flex;
+`
+
 
 
 export default class AboutPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
@@ -31,9 +36,9 @@ export default class AboutPage extends React.PureComponent { // eslint-disable-l
     return (
       <FullHeightSection bg_green vh_100 > 
         <Nav />
-        <Wrapper flex items_center w_100 h_75>
-          <Wrapper ml6_l ml5_m ml4 mt4 mt0_ns mb2 mb0_ns flex_row_ns flex  >
-            <Wrapper w_75 mr6 mr4_m >
+        <Wrapper flex items_center w_100 vh_75>
+          <FlexCont ml6_l ml5_m ml4 mt4 mt0_ns mb2 mb0_ns flex_row_ns flex  >
+            <Wrapper w_75 mr7_l mr4_m mr2 >
               <H1 f3 white> I am a designer. </H1>
               <BodyCopy measure_wide_ns measure fw2 white> Currently working for <BodyLink link washed_green href="https://www.fusetools.com/"> Fuse </BodyLink> in Oslo, Norway (I didn't do that site).<br/> We do some cool shit.<br/>Previously at <BodyLink link washed_green href="https://www.barclaysafrica.com/barclaysafrica/"> Barclays Africa </BodyLink>, <BodyLink link washed_green href="http://www.mavenagency.co.za/"> Maven Agency,</BodyLink> <BodyLink link washed_green href="http://www.levergy.co.za/"> Levergy </BodyLink> </BodyCopy>
               <Wrapper mt4>
@@ -41,9 +46,9 @@ export default class AboutPage extends React.PureComponent { // eslint-disable-l
               </Wrapper>       
             </Wrapper>
             <Wrapper dt_ns w_25 dn>
-                <Logo src={logo} height="200"  mr0 mr6_l mr4_m />
+                <Logo src={logo} height="200"  mr0 mr4_ns />
             </Wrapper>
-          </Wrapper>
+          </FlexCont>
         </Wrapper>
       </FullHeightSection>
     );
