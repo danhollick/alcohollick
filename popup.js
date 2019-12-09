182f859d5b0b8c271f51c856bc38d588d71faf16
Daniel Hollick <dan.hollick@gmail.com>
2019-12-09 13:25:01 +0100

    linting and cleanup

--- popup.js
+++ popup.js
@@ -1,44 +1,62 @@
-class popUp { 
-  constructor(company, year, pos, blurb,link,img) {
-  this.company = company
-  this.year = year
-  this.pos = pos
-  this.blurb = blurb
-  this.link = link
-  this.img = img
-}
-  
-  //create elements
-  create(){
-  reset()
-  var container = select('#container')
-  var mainDiv = createDiv().addClass('fixed z-2 bg-black-40 w-100 vh-100 flex items-center justify-center')
-  var card = createDiv().addClass('w-70 br2 vh-75 bg-white flex')
-  var imgBG = createDiv().addClass('w-50 h-100 br2 bg-near-white flex items-center justify-center ph4').style("background-image: url('assets/loading.png');background-repeat: no-repeat; background-position: center;background-size: 100px;")
-  var imgCont = createImg(this.img,"portfolio item").style("max-height: 450px")
-  var textCont = createDiv().addClass('pa4 w-50 flex items-center justify-center')
-  var textBound = createDiv()
-  var year = createElement('h2', this.year,).addClass('f2 fw8 moon-gray mb0')
-  var comp = createA(this.link, this.company,'_blank').addClass('f1 link fw8 dark-gray ma0 dim') 
-  var pos = createElement('h2', this.pos).addClass('f2 fw6 gray mt0')
-  var blurb = createP(this.blurb).addClass('f6 lh-copy word-wrap gray')
-  var btnCnt = createA('javascript:resetDiv()',"").addClass('link ml3')
-  var btnBG = createDiv().addClass('br2 w-25 flex items-center justify-center pointer grow o-80 glow').style('background-color: #F0484C;')
-  var btnTxt = createP("Done").addClass('mh3 f7 fw6 white')
+class popUp {
+  constructor(company, year, pos, blurb, link, img) {
+    this.company = company
+    this.year = year
+    this.pos = pos
+    this.blurb = blurb
+    this.link = link
+    this.img = img
+  }
 
-  //organise tree
-  container.child(mainDiv)
-  mainDiv.child(card)
-  card.child(imgBG)
-  imgBG.child(imgCont)
-  card.child(textCont)
-  textCont.child(textBound)
-  textBound.child(year)
-  textBound.child(comp)
-  textBound.child(pos)
-  textBound.child(blurb)
-  textBound.child(btnCnt)
-  btnCnt.child(btnBG)
-  btnBG.child(btnTxt)
+  // create elements
+  create() {
+    reset()
+    const container = select('#container')
+    const mainDiv = createDiv().addClass(
+      'fixed z-2 bg-black-40 w-100 vh-100 flex items-center justify-center'
+    )
+    const card = createDiv().addClass('w-70 br2 vh-75 bg-white flex')
+    const imgBG = createDiv()
+      .addClass(
+        'w-50 h-100 br2 bg-near-white flex items-center justify-center ph4'
+      )
+      .style(
+        "background-image: url('assets/loading.png');background-repeat: no-repeat; background-position: center;background-size: 100px;"
+      )
+    const imgCont = createImg(this.img, 'portfolio item').style(
+      'max-height: 450px'
+    )
+    const textCont = createDiv().addClass(
+      'pa4 w-50 flex items-center justify-center'
+    )
+    const textBound = createDiv()
+    const year = createElement('h2', this.year).addClass('f2 fw8 moon-gray mb0')
+    const comp = createA(this.link, this.company, '_blank').addClass(
+      'f1 link fw8 dark-gray ma0 dim'
+    )
+    const pos = createElement('h2', this.pos).addClass('f2 fw6 gray mt0')
+    const blurb = createP(this.blurb).addClass('f6 lh-copy word-wrap gray')
+    const btnCnt = createA('javascript:resetDiv()', '').addClass('link ml3')
+    const btnBG = createDiv()
+      .addClass(
+        'br2 w-25 flex items-center justify-center pointer grow o-80 glow'
+      )
+      .style('background-color: #F0484C;')
+    const btnTxt = createP('Done').addClass('mh3 f7 fw6 white')
+
+    // organise tree
+    container.child(mainDiv)
+    mainDiv.child(card)
+    card.child(imgBG)
+    imgBG.child(imgCont)
+    card.child(textCont)
+    textCont.child(textBound)
+    textBound.child(year)
+    textBound.child(comp)
+    textBound.child(pos)
+    textBound.child(blurb)
+    textBound.child(btnCnt)
+    btnCnt.child(btnBG)
+    btnBG.child(btnTxt)
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,11 +1,10 @@
-
-var rocket
-var blackhole
-var carrier
-var modulator
-var isMuted
-var someNoise
-var data
+let rocket
+let blackhole
+let carrier
+let modulator
+let isMuted
+let someNoise
+let data
 
 function preload() {
   rocket = loadImage('assets/Rocket.png')
@@ -17,99 +16,115 @@ function preload() {
 }
 
 function setup() {
-  if (displayWidth < displayHeight && displayWidth < 1025){
-    createCanvas(windowWidth, windowHeight);
-    var bgDiv = createDiv().addClass('absolute tc bg-red pa5 vh-100 w-100 flex items-center justify-center')
-    var cntDiv = createDiv()
-    var heading = createElement('h1', "The site requires a keyboard to run.").addClass('f1 fw6 white')
-    var info = createElement('h2', "(If you are seeing this and you do have a keyboard, it's because I am using your screen size to guess. So it's probably because your screen is quite small.)").addClass('f3 fw3 white measure')
+  if (displayWidth < displayHeight && displayWidth < 1025) {
+    createCanvas(windowWidth, windowHeight)
+    const bgDiv = createDiv().addClass(
+      'absolute tc bg-red pa5 vh-100 w-100 flex items-center justify-center'
+    )
+    const cntDiv = createDiv()
+    const heading = createElement(
+      'h1',
+      'The site requires a keyboard to run.'
+    ).addClass('f1 fw6 white')
+    const info = createElement(
+      'h2',
+      "(If you are seeing this and you do have a keyboard, it's because I am using your screen size to guess. So it's probably because your screen is quite small.)"
+    ).addClass('f3 fw3 white measure')
     // smlCanvas.parent('error')
     bgDiv.parent('#error')
     cntDiv.parent(bgDiv)
     heading.parent(cntDiv)
     info.parent(cntDiv)
     noLoop()
-  }
-  else {
-  createCanvas(windowWidth, windowHeight)
-  rectMode(CENTER)
-  imageMode(CENTER)
-  colorMode(HSB)
-  angleMode(DEGREES)
-  reset()
+  } else {
+    createCanvas(windowWidth, windowHeight)
+    rectMode(CENTER)
+    imageMode(CENTER)
+    colorMode(HSB)
+    angleMode(DEGREES)
+    reset()
 
-  carrier = new p5.Oscillator() // connects to master output by default
-  carrier.freq(340)
-  carrier.amp(0)
-  // carrier's amp is 0 by default, giving our modulator total control
-  carrier.start()
+    carrier = new p5.Oscillator() // connects to master output by default
+    carrier.freq(340)
+    carrier.amp(0)
+    // carrier's amp is 0 by default, giving our modulator total control
+    carrier.start()
 
-  modulator = new p5.Oscillator('triangle')
-  modulator.disconnect()  // disconnect the modulator from master output
-  modulator.freq(5)
-  modulator.amp(1)
-  modulator.start()
-  // Modulate the carrier's amplitude with the modulator
-  // Optionally, we can scale the signal.
-  carrier.amp(modulator.scale(-1,1,1,-1))
+    modulator = new p5.Oscillator('triangle')
+    modulator.disconnect() // disconnect the modulator from master output
+    modulator.freq(5)
+    modulator.amp(1)
+    modulator.start()
+    // Modulate the carrier's amplitude with the modulator
+    // Optionally, we can scale the signal.
+    carrier.amp(modulator.scale(-1, 1, 1, -1))
 
-  masterVolume(0.2)
-  isMuted = false
+    masterVolume(0.2)
+    isMuted = false
   }
 }
 
 function draw() {
-  background(206,39,14)
+  background(206, 39, 14)
   // for (var h = 0; h < numStars; h++) {
   //   stars[h].display()
   // }
-  for (var h = 0; h < numHoles; h++) {
+  for (let h = 0; h < numHoles; h++) {
     holes[h].contain(mover)
     holes[h].attract(mover)
     holes[h].display()
   }
-  for (var j = 0; j < numPlanets; j++) {
+  for (let j = 0; j < numPlanets; j++) {
     planets[j].display()
   }
 
   Steer()
-  //friction
+  // friction
   mover.update()
   mover.bump(planets)
   mover.display()
   mover.checkEdges()
 
   speed = mover.velocity.mag()
-  var panning = map(mover.position.x, 0., width,-1.0, 1.0)
+  const panning = map(mover.position.x, 0, width, -1.0, 1.0)
   carrier.pan(panning)
 
-  var modFreq = map(speed, 0, 10, 0, 60);
-  modulator.freq(modFreq);
+  const modFreq = map(speed, 0, 10, 0, 60)
+  modulator.freq(modFreq)
 
-  var modAmp = map(speed, 0.2, 10, 0.0, 1,true);
-  modulator.amp(modAmp, 0.01);
+  const modAmp = map(speed, 0.2, 10, 0.0, 1, true)
+  modulator.amp(modAmp, 0.01)
 }
 
 function reset() {
   numHoles = 7
   holes = []
-  numPlanets = windowWidth/20
+  numPlanets = windowWidth / 20
   planets = []
-  mover = new Mover(windowWidth/2,windowHeight/2,28,8,rocket)
+  mover = new Mover(windowWidth / 2, windowHeight / 2, 28, 8, rocket)
 
-
-  for (var l = 0; l < numHoles; l++) {
-    h = new Hole(150, l, data[l].company, data[l].year, data[l].position,data[l].blurb, data[l].link, data[l].img,blackhole)
+  for (let l = 0; l < numHoles; l++) {
+    h = new Hole(
+      150,
+      l,
+      data[l].company,
+      data[l].year,
+      data[l].position,
+      data[l].blurb,
+      data[l].link,
+      data[l].img,
+      blackhole
+    )
     holes.push(h)
     holes[l].place(holes)
   }
 
-  for (var k = 0; k < numPlanets; k++) {
-    var d = map(noise(k),0,1,8,32)
-    var s = map(noise(k),0,1,0,60)
-    var b = map(noise(k),0,1,40,100)
-    var h = map(noise(k),0,1,150,220)
-    p = new Planet(d, k + numHoles, h,s,b) // generate a random sized circObj and store it's ID for later
+  for (let k = 0; k < numPlanets; k++) {
+    const d = map(noise(k), 0, 1, 8, 32)
+    const s = map(noise(k), 0, 1, 0, 60)
+    const b = map(noise(k), 0, 1, 40, 100)
+    var h = map(noise(k), 0, 1, 150, 220)
+    p = new Planet(d, k + numHoles, h, s, b) // generate a random sized circObj and store it's ID for later
     planets.push(p)
     planets[k].place(planets)
     planets[k].place(holes)
@@ -133,23 +148,22 @@ function resetDiv() {
 
 function Steer() {
   if (keyIsDown(LEFT_ARROW)) {
-    var thrust = createVector(-0.2,0)
+    var thrust = createVector(-0.2, 0)
     mover.applyForce(thrust)
   }
 
   if (keyIsDown(RIGHT_ARROW)) {
-    var thrust = createVector(0.2,0)
+    var thrust = createVector(0.2, 0)
     mover.applyForce(thrust)
   }
 
   if (keyIsDown(UP_ARROW)) {
-    var thrust = createVector(0,-0.2)
+    var thrust = createVector(0, -0.2)
     mover.applyForce(thrust)
   }
 
   if (keyIsDown(DOWN_ARROW)) {
-    var thrust = createVector(0,0.2)
+    var thrust = createVector(0, 0.2)
     mover.applyForce(thrust)
   }
 }
-
