255cf8b17d8d3f9993e78c558cf669b5f6a45b16
Dan Hollick <dan.hollick@gmail.com>
2023-01-27 13:45:54 +0000

    supres Typescript errors

--- next.config.js
+++ next.config.js
@@ -16,6 +16,13 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
 
 // const withMDX = require('@next/mdx')({
